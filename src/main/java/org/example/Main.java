4395f75071121f56d2f555cb1fa0e8c55c61c424
IvanMaltcev <MalthevIU@mail.ru>
2024-12-16 19:25:23 +0300

    Task 2 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        String fullName = "Иванов Иван Иванович";
+        String[] partsOfName = fullName.split(" ");
+        String lastName = partsOfName[0];
+        String firstName = partsOfName[1];
+        String surname = partsOfName[2];
+        System.out.println(lastName);
+        System.out.println(firstName);
+        System.out.println(surname);
     }
 }
